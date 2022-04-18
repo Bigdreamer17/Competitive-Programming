77d80b39f29e3492ca33c49c5e77ec65c6a46d3a
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2022-04-18 13:32:32 +0300

    loop

---
+++ loop.py
@@ -0,0 +1,12 @@
+n = int(input("Enter number: "))
+
+if n >= 1 and n <= 20:
+        for i in range(n):
+            print (i * i)
+else:
+    print("out  of range ")
+    
+        
+
+        
+    
