dcc96df6da3f51fc795113dd9613d7954ecbdec0
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2022-04-29 23:00:48 +0300

    What's your name?

---
+++ Whats.py
@@ -0,0 +1,6 @@
+def print_full_name(Fname, Lname):
+    print("Hello", Fname, Lname + "!" , "You just delved into python.")
+if __name__ == '__main__':
+    First_name = input("Enter first name: ")
+    Last_name = input("Enter last name: ")
+    print_full_name(First_name,Last_name)
