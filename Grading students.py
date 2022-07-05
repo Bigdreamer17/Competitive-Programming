2c4a7e015f17ba85b1cd41f8ce6b9aba2e5e4a2a
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2022-07-05 21:06:26 +0300

    Grading Students

---
+++ Grading students.py
@@ -0,0 +1,25 @@
+import math
+
+grades_count = int(input().strip())
+
+grades = []  
+for _ in range(grades_count):
+    grades_item = int(input().strip())
+    grades.append(grades_item)
+        
+for i in range(len(grades)):
+    multiple = math.ceil(grades[i] / 5) * 5
+    result = multiple - grades[i]
+    
+    if  result < 3 and grades[i] >= 38:
+        grades[i] = multiple
+    if result >= 3:
+        grades[i] = grades[i]
+
+for i in grades:
+    print(i)
+
+73
+67
+38
+33
